b61f162785696be6f65b2db1f4520e3412110e0c
Bestin Lalu <46861674+bestinlalu@users.noreply.github.com>
2026-08-21 14:24:28 -0400

    Add: valid-tree - submission-6

---
+++ Data Structures & Algorithms/valid-tree/submission-6.py
@@ -0,0 +1,30 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+
+        if n <= 1:
+            return True
+
+        edgeMap = defaultdict(list)
+        visited, cycle = set(),set()
+
+        for v1, v2 in edges:
+            edgeMap[v1].append(v2)
+            edgeMap[v2].append(v1)
+
+        def dfs(pre, cur):
+            if cur in visited:
+                return False
+            if cur not in edgeMap:
+                return True
+            visited.add(cur)
+            for v2 in edgeMap[cur]:
+                if v2 != pre and not dfs(cur, v2):
+                    visited.remove(cur)
+                    return False
+            edgeMap[cur] = []
+            return True
+        
+        dfs(-1, 0)
+        return len(visited) == n
+
+        
