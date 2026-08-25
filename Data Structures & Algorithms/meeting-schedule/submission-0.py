5d970bc4399950260851bda0f5914bfa0eb8023f
Bestin Lalu <46861674+bestinlalu@users.noreply.github.com>
2026-08-25 17:03:35 -0400

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.py
@@ -0,0 +1,20 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+
+        intervals = sorted(intervals, key=lambda x: x.end)
+
+        i = 0
+        while i < len(intervals) - 1:
+            if intervals[i].end > intervals[i + 1].start:
+                return False
+            i += 1
+
+        return True
